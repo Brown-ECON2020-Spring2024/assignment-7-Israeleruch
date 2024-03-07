68571be437dc6a98a3610b6f9638f8ce7603a4a4
yourusername <youremail@brown.edu>
2024-03-07 18:35:57 -0500

    Added a test.

--- tests/test-example.r
+++ tests/test-example.r
@@ -17,3 +17,5 @@ test_that("Check the number of observations", {
 expect_unique(data = data, c(year, country))
 
 expect_range(data = data, gdpPercap, 0, 1e6)
+
+expect_values(data = data, continent, c("Asia", "Europe", "Africa", "Americas", "Oceania"))
